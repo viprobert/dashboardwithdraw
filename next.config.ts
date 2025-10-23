f2bf72d6f7daec22595707769f3d95f85b944e45
viprobert <vip.robert14@gmail.com>
2025-10-23 20:01:09 +0700

    fix again

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   output: "export",
-   eslint: { ignoreDuringBuilds: true }
+  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
